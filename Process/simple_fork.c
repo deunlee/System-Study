604c046ea37009e52d3415cc28a04ee0ab51c6ca
Deun Lee <54697735+deunlee@users.noreply.github.com>
2021-12-13 19:06:01 +0900

    Update simple_fork.c

--- Process/simple_fork.c
+++ Process/simple_fork.c
@@ -5,24 +5,32 @@
 #include <sys/wait.h>
 
 int parent() {
-    printf("[Parent] I am your father.");
+    printf("[Parent] I am your father.\n");
+    return 0;
 }
 
 int child() {
-    printf("[Child] I am child.");
+    printf("[Child] I am a child.\n");
+    return 123; // Return value is 8 bits.
 }
 
 int main() {
     pid_t pid = fork();
+    int ret, wstatus;
 
     if (pid < 0) {
         perror("[Parent] Failed to fork process");
         return 1;
     } else if (pid == 0) {
         printf("[Child] Forked successfully. (PID=%d, PPID=%d)\n", getpid(), getppid());
-        return child();
+        ret = child();
     } else {
         printf("[Parent] Forked successfully. (PID=%d)\n", getpid());
-        return parent();
+        ret = parent();
+        wait(&wstatus);
+        // https://man7.org/linux/man-pages/man2/wait.2.html
+        printf("[Parent] Child returned %d.\n", WEXITSTATUS(wstatus));
     }
+
+    return ret;
 }
