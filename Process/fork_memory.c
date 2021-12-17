0111a15125063aa2955f5e57f8fcd584a43f346a
Deun Lee <54697735+deunlee@users.noreply.github.com>
2021-12-17 11:22:20 +0900

    Create fork_memory.c

---
+++ Process/fork_memory.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int  global_var;  // DATA
+int* dynamic_var; // HEAP
+
+int main() {
+    pid_t pid;
+    int local_var; // STACK
+    
+    dynamic_var  = (int*)malloc(sizeof(int));
+    global_var   = 10;
+    local_var    = 20;
+    *dynamic_var = 30;
+    
+
+    printf("INIT: Default memory value. (global=DATA, local=STACK, dynamic=HEAP)\n");
+    printf("[Parent] global=%d (0x%p), local=%d (0x%p), dynamic=%d (0x%p)\n",
+            global_var, &global_var, local_var, &local_var, *dynamic_var, dynamic_var);
+
+
+    printf("\nFIRST: After fork, child process modifies memory.\n");
+    pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        exit(1);
+    } else if (pid == 0) {
+        global_var++;
+        local_var++;
+        (*dynamic_var)++;
+        printf("[Child]  global=%d (0x%p), local=%d (0x%p), dynamic=%d (0x%p)\n",
+                global_var, &global_var, local_var, &local_var, *dynamic_var, dynamic_var);
+        exit(0);
+    } else {
+        sleep(2);
+        printf("[Parent] global=%d (0x%p), local=%d (0x%p), dynamic=%d (0x%p)\n",
+                global_var, &global_var, local_var, &local_var, *dynamic_var, dynamic_var);
+    }
+
+
+    printf("\nSECOND: After fork, parent process modifies memory.\n");
+    pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        exit(1);
+    } else if (pid == 0) {
+        sleep(2);
+        printf("[Child]  global=%d (0x%p), local=%d (0x%p), dynamic=%d (0x%p)\n",
+                global_var, &global_var, local_var, &local_var, *dynamic_var, dynamic_var);
+        exit(0);
+    } else {
+        global_var++;
+        local_var++;
+        (*dynamic_var)++;
+        printf("[Parent] global=%d (0x%p), local=%d (0x%p), dynamic=%d (0x%p)\n",
+                global_var, &global_var, local_var, &local_var, *dynamic_var, dynamic_var);
+        sleep(4);
+    }
+
+    return 0;
+}
