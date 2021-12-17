f0c04caf296b2523d7873d6ac454eb0fd77cc18c
Deun Lee <54697735+deunlee@users.noreply.github.com>
2021-12-17 12:00:52 +0900

    Update server.c

--- Socket/01_Basic_TCP/server.c
+++ Socket/01_Basic_TCP/server.c
@@ -7,15 +7,15 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define BUFFER_SIZE  4096
-#define DEFAULT_IP   "0.0.0.0"
+#define BUFFER_SIZE   1024
 #define DEFAULT_PORT 55555
+#define DEFAULT_IP   "0.0.0.0"
 
 int main() {
     int sockfd, sockfd_accept;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_addr_size;
-    char buffer[BUFFER_SIZE] = { 0, };
+    char buffer_recv[BUFFER_SIZE] = { 0, };
     char buffer_send[] = "hello this is server!!";
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -31,29 +31,31 @@ int main() {
     }
 
     listen(sockfd, 1);
-    printf("[Server] Server is running at %s:%d\n", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
+    printf("[Server] Server is running at %s:%d\n",
+            inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
 
     client_addr_size = sizeof(struct sockaddr);
     sockfd_accept = accept(sockfd, (struct sockaddr*)&client_addr, &client_addr_size);
     if (sockfd_accept == -1) {
         printf("[Server] Failed accept a connection.\n");
+        close(sockfd);
         return 1;
     }
 
-    printf("[Server] ListenFD=%d, AcceptFD=%d\n", sockfd, sockfd_accept);
-    printf("[Server] ClientIP=%s, Port=%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+    printf("[Server] The client is connected. (%s:%d) (listen fd=%d, accept fd=%d)\n",
+            inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port),
+            sockfd, sockfd_accept);
 
     if (send(sockfd_accept, buffer_send, strlen(buffer_send) + 1, 0) == -1) {
         printf("[Server] Failed to send a message.\n");
         return 1;
     }
 
-    if (recv(sockfd_accept, buffer, BUFFER_SIZE, 0) == -1) {
+    if (recv(sockfd_accept, buffer_recv, BUFFER_SIZE, 0) == -1) {
         printf("[Server] Failed to receive a message.\n");
         return 1;
     }
-
-    printf("[Server] Received: %s\n", buffer);
+    printf("[Server] Received: %s\n", buffer_recv);
 
     close(sockfd_accept);
     close(sockfd);
