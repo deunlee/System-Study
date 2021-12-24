fa67fe704a3d1c123c3eaa04333681e8bd2511ca
Deun Lee <54697735+deunlee@users.noreply.github.com>
2021-12-24 10:38:14 +0900

    Update basic TCP code

--- Socket/01_Basic_TCP/client.c
+++ Socket/01_Basic_TCP/client.c
@@ -15,7 +15,7 @@ int main() {
     int sockfd;
     struct sockaddr_in server_addr;
     char buffer_recv[BUFFER_SIZE] = { 0, };
-    char buffer_send[] = "hello from client~";
+    char buffer_send[] = "Hello from the client~";
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0); // SOCK_STREAM == TCP
 
@@ -25,21 +25,24 @@ int main() {
     server_addr.sin_addr.s_addr = inet_addr(DEFAULT_IP);
 
     if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        printf("[Client] Cannot connect to the server.\n");
+        perror("[Error] Cannot connect to the server");
         return 1;
     }
     printf("[Client] Connected to %s:%d (sockfd=%d)\n", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port), sockfd);
 
     if (recv(sockfd, buffer_recv, BUFFER_SIZE, 0) == -1) {
-        printf("[Client] Failed to receive a message.\n");
+        perror("[Error] Failed to receive a message");
+        close(sockfd);
         return 1;
     }
     printf("[Client] Received: %s\n", buffer_recv);
 
     if (send(sockfd, buffer_send, strlen(buffer_send) + 1, 0) == -1) {
-        printf("[Client] Failed to send a message.\n");
+        perror("[Error] Failed to send a message");
+        close(sockfd);
         return 1;
     }
+    printf("[Client] Sent a message to the server.\n");
 
     close(sockfd);
     return 0;
--- Socket/01_Basic_TCP/server.c
+++ Socket/01_Basic_TCP/server.c
@@ -16,7 +16,7 @@ int main() {
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_addr_size;
     char buffer_recv[BUFFER_SIZE] = { 0, };
-    char buffer_send[] = "hello this is server!!";
+    char buffer_send[] = "Hello~ This is the server!!";
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0); // SOCK_STREAM == TCP
 
@@ -26,7 +26,7 @@ int main() {
     server_addr.sin_addr.s_addr = inet_addr(DEFAULT_IP);
 
     if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        printf("[Server] Cannot bind a socket.\n");
+        perror("[Error] Cannot bind a socket");
         return 1;
     }
 
@@ -36,20 +36,22 @@ int main() {
     client_addr_size = sizeof(client_addr);
     sockfd_accept = accept(sockfd, (struct sockaddr*)&client_addr, &client_addr_size);
     if (sockfd_accept == -1) {
-        printf("[Server] Failed accept a connection.\n");
+        perror("[Error] Failed accept a connection");
         close(sockfd);
         return 1;
     }
-    printf("[Server] The client is connected. (%s:%d) (listen fd=%d, accept fd=%d)\n",
+    printf("[Server] Client is connected. (%s:%d) (listen fd=%d, accept fd=%d)\n",
             inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), sockfd, sockfd_accept);
 
     if (send(sockfd_accept, buffer_send, strlen(buffer_send) + 1, 0) == -1) {
-        printf("[Server] Failed to send a message.\n");
+        perror("[Error] Failed to send a message");
+        close(sockfd);
         return 1;
     }
 
     if (recv(sockfd_accept, buffer_recv, BUFFER_SIZE, 0) == -1) {
-        printf("[Server] Failed to receive a message.\n");
+        perror("[Error] Failed to receive a message");
+        close(sockfd);
         return 1;
     }
     printf("[Server] Received: %s\n", buffer_recv);
